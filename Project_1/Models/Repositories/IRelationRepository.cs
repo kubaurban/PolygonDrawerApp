f323c5dd0cead995c132862160a6a485ce9cc9b2
Jakub Urbański <kuba.urban01@gmail.com>
2022-10-06 00:49:30 +0200

    add repository methods declarations

--- Project_1/Models/Repositories/IRelationRepository.cs
+++ Project_1/Models/Repositories/IRelationRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Project_1.Models.Repositories
+﻿using Project_1.Models.Relations;
+using System.Collections.Generic;
+
+namespace Project_1.Models.Repositories
 {
     public interface IRelationRepository
     {
+        public List<Relation> GetAll();
+        public Relation GetRelationById(int id);
+        public Relation Add(Relation relation);
+        public Relation Remove(int id);
+        public bool Remove(Relation relation);
     }
 }
--- Project_1/Models/Repositories/IShapeRepository.cs
+++ Project_1/Models/Repositories/IShapeRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace Project_1.Models.Repositories
+﻿using Project_1.Models.Shapes;
+using System.Collections.Generic;
+
+namespace Project_1.Models.Repositories
 {
     public interface IShapeRepository
     {
+        public List<Polygon> GetAllPolygons();
+        public Polygon GetPolygonById(int id);
+        public Polygon AddPolygon(Polygon polygon);
+        public Polygon RemovePolygon(int id);
+        public bool RemovePolygon(Polygon polygon);
+
+        public List<Point> GetSolitaryPoints();
+        public Point GetSolitaryPointById(int id);
+        public Point AddSolitaryPoint(Point point);
+        public void ClearSolitaryPoints();
     }
 }
