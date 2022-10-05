b63c2fa27db89f286e85174323c54e3b7e25c14f
Jakub Urbański <kuba.urban01@gmail.com>
2022-10-06 00:49:41 +0200

    implement repositories

--- Project_1/Models/Repositories/RelationRepository.cs
+++ Project_1/Models/Repositories/RelationRepository.cs
@@ -1,10 +1,31 @@
 ﻿using Project_1.Models.Relations;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Project_1.Models.Repositories
 {
     public class RelationRepository : IRelationRepository
     {
         private List<Relation> Relations { get; set; }
+
+        public Relation Add(Relation relation)
+        {
+            Relations.Add(relation);
+            return relation;
+        }
+
+        public List<Relation> GetAll() => Relations.ToList();
+
+        public Relation GetRelationById(int id) => Relations.Find(x => x.Id == id);
+
+        public Relation Remove(int id)
+        {
+            var relation = GetRelationById(id);
+            Remove(relation);
+
+            return relation;
+        }
+
+        public bool Remove(Relation relation) => Relations.Remove(relation);
     }
 }
--- Project_1/Models/Repositories/ShapeRepository.cs
+++ Project_1/Models/Repositories/ShapeRepository.cs
@@ -1,11 +1,44 @@
 ﻿using Project_1.Models.Shapes;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Project_1.Models.Repositories
 {
     public class ShapeRepository : IShapeRepository
     {
         private List<Polygon> Polygons { get; set; }
-        private List<Point> Points { get; set; }
+        private List<Point> SolitaryPoints { get; set; }
+
+        public Polygon AddPolygon(Polygon polygon)
+        {
+            Polygons.Add(polygon);
+            return polygon;
+        }
+
+        public Point AddSolitaryPoint(Point point)
+        {
+            SolitaryPoints.Add(point);
+            return point;
+        }
+
+        public void ClearSolitaryPoints() => SolitaryPoints.Clear();
+
+        public List<Polygon> GetAllPolygons() => Polygons.ToList();
+
+        public Polygon GetPolygonById(int id) => Polygons.Find(x => x.Id == id);
+
+        public Point GetSolitaryPointById(int id) => SolitaryPoints.Find(x => x.Id == id);
+
+        public List<Point> GetSolitaryPoints() => SolitaryPoints.ToList();
+
+        public Polygon RemovePolygon(int id)
+        {
+            var polygon = GetPolygonById(id);
+            RemovePolygon(polygon);
+
+            return polygon;
+        }
+
+        public bool RemovePolygon(Polygon polygon) => Polygons.Remove(polygon);
     }
 }
