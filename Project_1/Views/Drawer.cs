4304ffb7adaeef50d5425fd1e0a041d510495242
Jakub Urbański <kuba.urban01@gmail.com>
2022-10-07 20:20:36 +0200

    define basic IDrawer interface methods and implement them in Drawer class

--- Project_1/Views/Drawer.cs
+++ Project_1/Views/Drawer.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using Project_1.Models.Shapes;
+using System;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
+using Point = Project_1.Models.Shapes.Point;
 
 namespace Project_1.Views
 {
     public partial class Drawer : Form, IDrawer
     {
+        private readonly Bitmap _drawArea;
+        private readonly Pen _blackPen;
+        private readonly Brush _blackBrush;
+        private readonly float _pointWidth;
+
+        public Bitmap DrawArea => _drawArea;
+        public Graphics Graphics => Graphics.FromImage(DrawArea);
+        public Pen BlackPen => _blackPen;
+        public Brush BlackBrush => _blackBrush;
+        public float PointWidth => _pointWidth;
+
         public Drawer()
         {
             InitializeComponent();
+
+            _drawArea = new Bitmap(PictureBox.Width, PictureBox.Height);
+            _blackPen = new Pen(Color.Black);
+            _blackBrush = new SolidBrush(Color.Black);
+            _pointWidth = 0.25f;
+
+            PictureBox.Image = DrawArea;
+            ClearArea();
+            RefreshArea();
         }
 
         private void DeleteModeChecked(object sender, EventArgs e)
@@ -18,6 +42,45 @@ namespace Project_1.Views
         private void DrawingModeChecked(object sender, EventArgs e)
         {
             IsBresenham.Enabled = true;
+        }
+
+        public void DrawLine(Point p1, Point p2)
+        {
+            using var g = Graphics;
+            g.DrawLine(BlackPen, p1, p2);
+        }
+
+        public void DrawPoint(Point p)
+        {
+            using var g = Graphics;
+            g.FillRectangle(BlackBrush, p.X - PointWidth / 2, p.Y - PointWidth / 2, PointWidth, PointWidth);
+        }
+
+        public void DrawPolygon(Polygon polygon)
+        {
+            using var g = Graphics;
+
+            var first = polygon.Vertices.First();
+            DrawPoint(first);
+            var prev = first;
+            foreach (var v in polygon.Vertices.Skip(1))
+            {
+                DrawPoint(v);
+                DrawLine(prev, v);
+                prev = v;
+            }
+            DrawLine(prev, first);
+        }
+
+        public void ClearArea()
+        {
+            using var g = Graphics;
+            g.Clear(Color.White);
+        }
+
+        public void RefreshArea()
+        {
+            PictureBox.Refresh();
         }
     }
 }
--- Project_1/Views/IDrawer.cs
+++ Project_1/Views/IDrawer.cs
@@ -1,6 +1,13 @@
+using Project_1.Models.Shapes;
+
 ﻿namespace Project_1.Views
 {
     public interface IDrawer
     {
+        public void DrawLine(Point p1, Point p2);
+        public void DrawPoint(Point p);
+        public void DrawPolygon(Polygon polygon);
+        public void ClearArea();
+        public void RefreshArea();
     }
 }
