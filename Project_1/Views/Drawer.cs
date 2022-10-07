dabba25a7d30858b68d13f3adba0cd1c044a194c
Jakub Urbański <kuba.urban01@gmail.com>
2022-10-07 14:11:51 +0200

    add logic between modes and IsBresenham checkbox

--- Project_1/Views/Drawer.cs
+++ Project_1/Views/Drawer.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace Project_1.Views
 {
@@ -7,6 +8,16 @@ namespace Project_1.Views
         public Drawer()
         {
             InitializeComponent();
+        }
+
+        private void DeleteModeChecked(object sender, EventArgs e)
+        {
+            IsBresenham.Enabled = false;
+        }
+
+        private void DrawingModeChecked(object sender, EventArgs e)
+        {
+            IsBresenham.Enabled = true;
         }
     }
 }
